c61101aeae180464c4ed47fe17356ed41bb3087c
charles <cfprice3@gmail.com>
2020-02-10 22:51:51 -0500

    created variables for js page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLower = "abcdefghijklmnopqrstuvwxyz";    
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*(){}[]=<>/.,";
+var characters = "";
+
+  
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
